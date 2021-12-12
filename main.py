60d42bfd0798e25476bfec4e40690d62309142f9
Rafael Rabelo <52937689+RafaelMR86@users.noreply.github.com>
2021-12-12 19:56:28 -0300

    master

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord 
+import os
+
+os.environ['TOKEN'] = 'bot-token'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in! Go on {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$fala'):
+        await message.channel.send('Meteu essa???????!!!!!')
+
+client.run(os.environ.get('TOKEN'))
